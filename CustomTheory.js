16b0f7f8c259e6383823b04f3f0e905eba18f427
Mustafa Jalalzai <must946@gmail.com>
2022-04-30 02:30:26 +0200

    revert more

--- CustomTheory.js
+++ CustomTheory.js
@@ -449,14 +449,13 @@ var postPublish = () => {
     secret_count = 0;
 }
 
-var getInternalState = () => `${num_publish} ${q} ${t} ${scale}`
+var getInternalState = () => `${num_publish} ${q} ${t}`
 
 var setInternalState = (state) => {
     let values = state.split(" ");
-    if (values.length > 0) num_publish = parseInt(values[0]);
+    if (values.length > 0) q = parseInt(values[0]);
     if (values.length > 1) q = parseBigNumber(values[1]);
     if (values.length > 2) t = parseBigNumber(values[2]);
-    if (values.length > 3) scale = parseBigNumber(values[3]);
     theory.clearGraph();
     state.x = t_graph.toNumber();
     state.y = R.toNumber();
